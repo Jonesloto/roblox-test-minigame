a5ccc320f3d19116012749f49261c0e467e26dce
Jonesloto <70425494+Jonesloto@users.noreply.github.com>
2020-10-05 20:58:10 -0400

    Adding "player has just joined the game" to game testing service

--- src/ServerScriptService/Server (Jonii Framework v2)/Source/Services/GameTestingService.lua
+++ src/ServerScriptService/Server (Jonii Framework v2)/Source/Services/GameTestingService.lua
@@ -2,6 +2,10 @@ local GameTestingService = {}
 
 function GameTestingService:start()
 	print("Hello World!")
+
+	self.Players.PlayerAdded:Connect(function(player)
+		print(player.Name .. " has just joined the game!")
+	end)
 end
 
 return GameTestingService
